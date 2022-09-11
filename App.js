c61fb343620b1d277fb3c22380b2a314185b6ef6
Luciano Weslen <luciano.weslen@zrp.com.br>
2022-09-11 09:02:10 -0300

    feat: added record audio and playback

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+
+  async function startRecording() {
+    try {
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    
+    const file = recording.getURI(); 
+    const duration = getDurationFormatted(status.durationMillis); 
+
+    updatedRecordings.push({
+      sound,
+      duration,
+      file
+    });
+
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millis) {
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay}:${secondsDisplay}`;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title={recording ? 'Parar gravação' : 'Iniciar gravação'}
+        onPress={recording ? stopRecording : startRecording}
+      />
+      {recordings.map((recordingLine, index) => {
+        return (
+          <View key={index} style={styles.row}>
+            <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+            <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Reproduzir"></Button>
+          </View>
+        );
+      })} 
     </View>
   );
 }
@@ -17,4 +76,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fill: {
+    flex: 1,
+    margin: 16
+  },
+  button: {
+    margin: 16
+  }
 });
